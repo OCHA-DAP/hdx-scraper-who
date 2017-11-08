b804895dad03989ea87eeb176800df0e4f6cb752
Michael Rans <rans@email.com>
2017-11-08 09:52:57 +0100

    Add emailing

--- run.py
+++ run.py
@@ -7,13 +7,14 @@ Top level script. Calls other functions that generate datasets that this script 
 import logging
 from os.path import join
 
-from hdx.facades import logging_kwargs
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
 
 from who import generate_dataset_and_showcase, get_countriesdata, get_indicators_and_tags
 
+from hdx.facades import logging_kwargs
 logging_kwargs['smtp_config_yaml'] = join('config', 'smtp_configuration.yml')
+
 from hdx.facades.hdx_scraperwiki import facade
 
 logger = logging.getLogger(__name__)
