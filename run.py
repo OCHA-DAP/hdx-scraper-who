2f2cae9955251b155e9c34c99a5cb0b8a7a2e911
Mike <rans@email.com>
2018-08-29 13:49:27 +0200

    Use .hdx_configuration.yml

--- run.py
+++ run.py
@@ -43,5 +43,5 @@ def main():
 
 
 if __name__ == '__main__':
-    facade(main, hdx_site='feature', user_agent_config_yaml=join(expanduser('~'), '.useragents.yml'), user_agent_lookup='hdxscraper-who', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, user_agent_config_yaml=join(expanduser('~'), '.useragents.yml'), user_agent_lookup='hdxscraper-who', project_config_yaml=join('config', 'project_configuration.yml'))
 
